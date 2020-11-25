cf7fe144837a0a792b4b699b78a44f56054d28f8
sravanichinna <sravanidane413.com>
2020-11-25 14:12:32 +0530

    Added: EmployeeWageByUsingClassMethod

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,37 +1,37 @@
-import java.util.Random;
 public class empWageComputation{
-	public static void main(String args[]) 
-	{
-	System.out.println("Welcome to employee wage computation");
-		int maxHrsInMonth=100;
-		int noOfWorkDay=20;
-		int empRatePerHr=20;
-		int isPartTime=1;
-		int isFullTime=2;
-		int empHrs=0;
-		int totalEmpHrs=0;
-		int totalWorkDay=0;
-		int totalEmpWage=0;
-	while( totalWorkDay<noOfWorkDay && totalEmpHrs<=maxHrsInMonth)
-	{
-		 Random random=new Random();
-                int randomNum=random.nextInt(3);
-                System.out.println("Random number: "+randomNum);
-	switch(randomNum)
-	{
-	case isPartTime: empHrs=4;
-		break;
-	case isFullTime: empHrs=8;
-		break;
-	default; empHrs=0;
-	}
-		totalWorkDays++;
-		totalEmpHrs=totalEmpHrs + empHrs;
-		System.out.println("employee days: "+totalWorkDay + "work hours: "+totalEmpHrs);
-	}
-totalEmpWage=(toatalEmpHrs * empRatePerHour);
-System.out.println("employee total salary is: "+totalEmpWage);
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=2;
+	public static final int MAX_HRS_IN_MONTH=10;
 
-
+	public static int computeEmpWage()
+	{
+	int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+	while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+	{
+		totalWorkingDays++;
+		int empCheck=(int) Math.floor(Math.random() * 10)%3;
+		switch(empCheck)
+		{
+		case IS_PART_TIME:
+			empHrs=4;
+			break;
+		case IS_FULL_TIME:
+			empHrs=8;
+			break;
+		default:
+			empHrs=0;
+		}
+		totalEmpHrs+=empHrs;
+		System.out.println("day: "+totalWorkingDays + "EmpHrs: "+empHrs);
+		}
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+		System.out.println("totak emp wage: "+totalWage);
+		return totalEmpWage;
+		}
+	public static void main(String[] args){
+	computeEmpWage();
 	}
 }
+
