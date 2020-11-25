e051c873aeb3497a3ae456fe297366747dc84f8f
sravanichinna <sravanidane413.com>
2020-11-25 13:20:26 +0530

    Added: DailyEmployeeWage logic

--- empWageComputation.java
+++ empWageComputation.java
@@ -3,17 +3,21 @@ public class empWageComputation{
 	public static void main(String args[]) 
 	{
 	System.out.println("Welcome to employee wage computation");
-	Random random=new Random();
+		Random random=new Random();
                 int randomNum=random.nextInt(2);
                 System.out.println("Random number: "+randomNum);
                 int isPresent=1;
                 if(randomNum==isPresent)
                 {
                         System.out.println("employee is present");
+            		int empHrs=8;
+                        int ratePerHour=20;
+                        System.out.println("salary== "+(empHrs*ratePerHour));
                 }
                 else
                 {
                         System.out.println("employee is absent");
+                        System.out.println("salary==0");
                 }
 	}
 }
